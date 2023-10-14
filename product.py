5006d907658e336245ca0a7bfde91428d0670b21
CheeseLesss <144573039+CheeseLesss@users.noreply.github.com>
2023-10-13 20:56:54 -0400

    Update product.py

--- product.py
+++ product.py
@@ -3,3 +3,39 @@ class Product:
     def __init__(self, exDate, Quant):
         self.exDate = exDate
         self.Quant = Quant
+
+class Food(Product):
+    pass
+
+class Dish(Food):
+    pass
+
+class Pizza(Dish):
+    pass
+
+class Cheese(Pizza):
+    pass
+
+class Peperoni(Pizza):
+    pass
+
+class Pineapple(Pizza):
+    pass
+
+class Calzone(Pizza):
+    pass
+
+class Side(Food):
+    pass
+
+class GarlicBread(Side):
+    pass
+
+class Salad(Side):
+    pass
+
+class House(Salad):
+    pass
+
+class Caesar(salad):
+    pass
